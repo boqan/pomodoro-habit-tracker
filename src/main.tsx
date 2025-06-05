0a35ddc4d86740b3ab18253e450aa9533de1d945
Boyan Atanasov <bojan.atanasov2002@gmail.com>
2025-06-05 22:24:37 +0300

    Autosave fixes

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// Debug code to verify React instance (keeping for now)
-window.React1 = React;
-console.log('React1 from main.tsx:', window.React1);
-
 const rootElement = document.getElementById("root");
 if (!rootElement) {
   throw new Error("Root element not found");
